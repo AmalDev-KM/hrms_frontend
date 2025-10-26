7f3cb76603eb72476b23c8addfcb58c723d2972d
AmalDev K M <amaldevmohan@gmail.com>
2025-10-26 22:00:52 +0530

    initial commit

---
+++ app/(admin)/(nonlayout)/login/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Hai Login</div>;
+};
+
+export default page;
---
+++ components/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card } from '../ui/card'
+
+const SigninComponent = () => {
+  return (
+    <div className='w-full min-h-screen'>
+      <Card>
+        
+      </Card>
+    </div>
+  )
+}
+
+export default SigninComponent
